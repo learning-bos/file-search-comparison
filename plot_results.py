48454f0e208a4cd761692f6711ef6afc396e0f99
eboss-dev <55625101+eboss-dev@users.noreply.github.com>
2024-08-30 12:35:20 +0200

    created images faster and modifying plot

--- plot_results.py
+++ plot_results.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # File paths of the CSVs
-csv_files = ["./go/go-results.csv", "./rust/rust-results.csv", "./cpp/cpp-results.csv"]
+csv_files = ["./rust/rust-results-optmized-no-lowercase.csv" ,"./go/go-results.csv", "./rust/rust-results-non-optimized.csv", "./cpp/cpp-results.csv"]
 
 # List to store the data from the first column of each CSV
 data_to_plot = []
@@ -19,7 +19,7 @@ plt.boxplot(data_to_plot)  # Plot the boxplot
 plt.xlabel("Languages used")  # Set the x-axis label
 plt.ylabel("milliseconds")  # Set the y-axis label
 plt.title("100 tries for each with needle in the end")  # Set the title
-plt.xticks([1, 2, 3], ["GO", "Rust", "C++"])  # Set x-axis ticks and labels
+plt.xticks([1, 2, 3, 4], ["Rust optimized", "GO", "Rust", "C++"])  # Set x-axis ticks and labels
 
 # Show the plot
 plt.show()
